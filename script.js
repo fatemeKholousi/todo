7c12a233d9dff6e79fe1196797b0587c92895d34
fateme kholousi <fatemekholousizaer@gmail.com>
2023-06-05 17:01:59 +0330

    fix : add item function created

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const itemInput=document.getElementById('item-input')
 document.getElementById('add-item-btn').addEventListener('click',addItemHandler)
 // add Item
-function addItemHandler(){}
+function addItemHandler(event){
+    event.preventDefault()
+    listElement=document.getElementById('item-list')
+    const liElement=document.createElement('li')
+    liElement.innerHTML=itemInput.value
+    listElement.appendChild(liElement)
+}
 // remove Item
 // filter
 // save in local storage
